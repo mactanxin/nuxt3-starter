9395baa692fcee41b088804740fbc58a4dc72ee2
Xin Tan <mactanxin@gmail.com>
2023-07-07 14:24:37 +0800

    feat(index page): add example counter usage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+const counter = useCounter()
+function clickme() {
+  console.log('click');
+}
+</script>
 
 <template>
   <h2>Hello Index</h2>
-  <v-btn color="primary" variant="outlined">Primary</v-btn>
+  <v-btn @click="clickme" color="primary" variant="outlined">Primary</v-btn>
+
+  <div>
+    counter value: {{ counter.n }}
+  </div>
+  <v-btn @click="counter.increment()" color="primary">+1</v-btn>
+
+  <pre>
+    {{ counter.$state }}
+  </pre>
 </template>
