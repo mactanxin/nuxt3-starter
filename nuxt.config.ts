1993aeb0af33b41cbf8df4d00f57a839bd31f7a4
Xin Tan <mactanxin@gmail.com>
2023-07-07 14:01:46 +0800

    feat(store): add nuxt store config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@invictus.codes/nuxt-vuetify'
+    '@invictus.codes/nuxt-vuetify',
+    '@pinia/nuxt'
   ],
+  imports: {
+    dirs: ['./stores'],
+  },
+  pinia: {
+    autoImports: ['defineStore', 'acceptHMRUpdate'],
+  },
   devtools: { enabled: true },
 })
