a6d3893abf4956fe34cc1809ec3d1a989fc3748e
Xin Tan <mactanxin@gmail.com>
2023-07-12 17:14:34 +0800

    feat(posts.vue): add posts layout

---
+++ layouts/posts.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/posts/recent">Recent</NuxtLink>
+      <NuxtLink to="/posts/popular">Popular</NuxtLink>
+      <NuxtLink to="/posts/trending">Trending</NuxtLink>
+    </nav>
+    <slot />
+  </div>
+</template>
