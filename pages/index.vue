0a11e3dff1c7a78e4f49266be6858dfe85b3c86d
Xin Tan <mactanxin@gmail.com>
2023-08-04 16:01:16 +0800

    feat(index): request api demo

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,19 @@ const counter = useCounter()
 function clickme() {
   console.log('click');
 }
+
+const { data } = await useFetch('/api/ninja?name=John', {
+  method: 'POST',
+  body: {
+    age: 30
+  }
+})
+console.log('ninja data: ',
+  data.value.message)
 </script>
 
 <template>
+  {{ data }}
   <h2>Hello Index</h2>
   <v-btn @click="clickme" color="primary" variant="outlined">Primary</v-btn>
 
@@ -13,6 +23,7 @@ function clickme() {
     counter value: {{ counter.n }}
   </div>
   <v-btn @click="counter.increment()" color="primary">+1</v-btn>
+  <v-btn @click="counter.decrementToZero()" color="primary">reset</v-btn>
 
   <pre>
     {{ counter.$state }}
